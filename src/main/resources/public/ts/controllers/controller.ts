d9bb02283fcfb54a558a1480b770e7d932932a69
alicedraillardcgi <alice.draillard@cgi.com>
2021-09-21 14:57:08 +0200

    feat(form): #MC-31 change share button name depending on calendar shared or not

--- src/main/resources/public/ts/controllers/controller.ts
+++ src/main/resources/public/ts/controllers/controller.ts
@@ -817,6 +817,16 @@ export const calendarController =  ng.controller('CalendarController',
         };
     }
 
+    $scope.nameOfShareButton = (calendarEvent) : string => {
+        let numberOfSharedCalendars = calendarEvent.calendar
+            .filter((calendar:Calendar): boolean => calendar.shared && (calendar.shared.length !== 0))
+            .length;
+
+        return (numberOfSharedCalendars === 0)?
+            lang.translate('calendar.event.save.and.share') :
+            lang.translate('calendar.event.save.and.restrict');
+    }
+
     $scope.createCalendarEvent = newItem => {
         $scope.calendarAsContribRight = new Array<String>();
         $scope.selectedCalendarInEvent = new Array<String>();
