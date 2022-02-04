c22c8b22ccf4ef377fbde1896c5e9d1f29d277fe
alicedraillardcgi <alice.draillard@cgi.com>
2022-02-04 17:57:50 +0100

    fix(dates): #MC-66 handle and secure invalid dates

--- src/main/resources/public/ts/utils/multiDaysEventsUtils.ts
+++ src/main/resources/public/ts/utils/multiDaysEventsUtils.ts
@@ -18,12 +18,12 @@ export class multiDaysEventsUtils {
         while (moment(multiDayEvent.endMoment).isSameOrAfter(temporaryEventDate, 'day')){
             let temporaryEvent : CalendarEvent = new CalendarEvent(multiDayEvent);
             //all days except the first day of the event
-            if (!multiDaysEventsUtils.isEventMultiDays(moment(multiDayEvent.startMoment), temporaryEventDate)){
+            if (!multiDaysEventsUtils.isEventOneDay(moment(multiDayEvent.startMoment), temporaryEventDate)){
                 temporaryEvent.startMoment = moment(temporaryEventDate).hours(timeConfig.start_hour).minutes(0).second(0).millisecond(0)
                     .utc().format(FORMAT.formattedISODate);
             }
             //all days except the last day of the event
-            if (!multiDaysEventsUtils.isEventMultiDays(moment(multiDayEvent.endMoment), temporaryEventDate)){
+            if (!multiDaysEventsUtils.isEventOneDay(moment(multiDayEvent.endMoment), temporaryEventDate)){
                 temporaryEvent.endMoment = moment(temporaryEventDate).hours(timeConfig.end_hour).minutes(0).second(0).millisecond(0)
                     .utc().format(FORMAT.formattedISODate);
             }
@@ -34,8 +34,12 @@ export class multiDaysEventsUtils {
         return dividedEvent;
     };
 
-    static isEventMultiDays = (startDate : Moment, endDate : Moment) : boolean => {
-        return startDate.isSame(endDate, 'day');
+    /**
+     * Returns true is the start and end date of the event are the same day or if one of them is not valid
+     * Events with invalid dates are treated like one day events
+     */
+    static isEventOneDay = (startDate : Moment, endDate : Moment) : boolean => {
+        return (!(moment(startDate).isValid()) || !(moment(endDate).isValid()) ||  startDate.isSame(endDate, 'day'));
     };
 
 
