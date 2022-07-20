cbe24a37429fa0784aa27b68eaf65c1c064dc861
adrienFro <93270851+adrienFro@users.noreply.github.com>
2022-07-20 17:05:37 +0200

    feat(delete): #MC-152 change date format(#96)(#97)

--- src/main/resources/public/ts/model/rbs/rbs-emitter.model.ts
+++ src/main/resources/public/ts/model/rbs/rbs-emitter.model.ts
@@ -41,8 +41,8 @@ export class RbsEmitter {
         let finalSlots: Array<string> = [];
         this.scope.calendarEvent.bookings.forEach((booking: SavedBooking) => {
             let bookingDates: string = "";
-            bookingDates += moment(moment.utc(booking.start_date, FORMAT.displayDateTimeShortYear).toDate()).format(FORMAT.formattedFRDateTimeNoSeconds)
-                + " - " + moment(moment.utc(booking.end_date, FORMAT.displayDateTimeShortYear).toDate()).format(FORMAT.formattedFRDateTimeNoSeconds);
+            bookingDates += moment(moment.utc(booking.start_date, FORMAT.displayDateTimeShortYear).toDate()).format(FORMAT.displayFRDate + " : " + FORMAT.displayTime)
+                + " - " + moment(moment.utc(booking.end_date, FORMAT.displayDateTimeShortYear).toDate()).format(FORMAT.displayTime);
 
             finalSlots.push(bookingDates);
         })
