7ca0f34b315908a4fc90bf67f34410d192604d46
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-07-27 16:05:13 +0200

    fix(rbs): #MC-164 secure rbs calls (#102)

---
+++ src/main/resources/public/ts/core/enum/rbs/rbs-eventer.enum.ts
@@ -0,0 +1,6 @@
+export enum RBS_EVENTER {
+    INIT_BOOKING_INFOS = 'initBookingInfos',
+    UPDATE_BOOKING_INFOS = 'updateBookingInfos',
+    CLOSE_BOOKING_INFOS = 'closeBookingInfos',
+    CAN_EDIT_EVENT = "canEditEvent"
+}
