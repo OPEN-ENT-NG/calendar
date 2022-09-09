f88e53f202bd1bd7ce043711109c47386d28786a
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-09-09 14:47:42 +0200

    feat(view): #MC-165 init external calendar import view (#119)

--- src/main/resources/public/ts/app.ts
+++ src/main/resources/public/ts/app.ts
@@ -1,6 +1,7 @@
 import { routes, ng } from "entcore";
 import * as controllers from './controllers';
 import {colorSelector, formatInputTime, sideBar, rbsBookingDeleteInfo} from './directives';
+import {calendarItem} from "./directives/calendar-item/calendar-item";
 
 for (let controller in controllers) {
     ng.controllers.push(controllers[controller]);
@@ -10,6 +11,7 @@ ng.directives.push(colorSelector);
 ng.directives.push(formatInputTime);
 ng.directives.push(sideBar);
 ng.directives.push(rbsBookingDeleteInfo);
+ng.directives.push(calendarItem);
 
 routes.define(function($routeProvider) {
     $routeProvider
