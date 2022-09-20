c403b7600e5d9a68ea5284282330ce9fe92cc07a
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-09-20 17:11:30 +0200

    feat(view): #MC-167 add external calendar form (#123 & #129)

--- src/main/resources/public/ts/directives/index.ts
+++ src/main/resources/public/ts/directives/index.ts
@@ -2,3 +2,6 @@ export * from './formatInputTime';
 export * from './colorSelector';
 export * from './side-bar/side-bar';
 export * from './rbs-booking-delete-info/rbs-booking-delete-info';
+export * from './calendar-item/calendar-item';
+export * from './external-calendar-addition/external-calendar-addition';
+export * from './external-calendar-form/external-calendar-form';
