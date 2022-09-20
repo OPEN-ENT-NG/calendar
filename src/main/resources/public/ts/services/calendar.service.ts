95a0324518a4f86ae1eec397834fdcd5de83cc6d
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-09-20 17:11:30 +0200

    feat(view): #MC-167 add external calendar form (#123 & #129)

--- src/main/resources/public/ts/services/calendar.service.ts
+++ src/main/resources/public/ts/services/calendar.service.ts
@@ -1,15 +1,34 @@
 import {ng} from 'entcore'
 import http, {AxiosResponse} from "axios";
 import {Calendar} from "../model";
+import {ICalendarPayload} from "../model/calendar-form.model";
 
 export interface ICalendarService {
     fetchCalendars(): Promise<Array<Calendar>>;
+    addExternalCalendar(calendar: Calendar): Promise<AxiosResponse>;
+    updateExternalCalendar(calendar: Calendar): Promise<AxiosResponse>;
 }
 
 export const calendarService: ICalendarService = {
     fetchCalendars(): Promise<Array<Calendar>> {
         return http.get(`/calendar/calendars`).then((response: AxiosResponse) =>
             response.data.map(calendar => new Calendar(calendar)));
+    },
+
+    async addExternalCalendar(calendar: ICalendarPayload): Promise<AxiosResponse> {
+        let urlParam: string = '';
+        if (calendar.icsLink) {
+            urlParam = `?url=${calendar.icsLink}`;
+        }
+        return http.post(`/calendar/calendars${urlParam}`, calendar);
+    },
+
+    updateExternalCalendar(calendar: Calendar): Promise<AxiosResponse> {
+        let urlParam: string = '';
+        if (calendar.icsLink) {
+            urlParam = `?url=${calendar.icsLink}`;
+        }
+        return http.put(`/calendar/${calendar._id}/ical${urlParam}`, calendar);
     }
 };
 
