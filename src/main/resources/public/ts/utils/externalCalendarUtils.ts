9e4a922cedaceeebf1baf7f921da0ea5b16a0ca7
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-09-22 14:47:42 +0200

    feat(events): #MC-169 remove external calendar edition possibilities (#124 & #131)

---
+++ src/main/resources/public/ts/utils/externalCalendarUtils.ts
@@ -0,0 +1,12 @@
+import {Calendar, CalendarEvent} from "../model";
+
+
+export class externalCalendarUtils {
+
+    /**
+     * Returns true if the calendar is external
+     */
+    static isCalendarExternal = (cal: Calendar): boolean => {
+        return !!(cal.isExternal && cal.icsLink && cal.icsLinkPlatform);
+    }
+}
