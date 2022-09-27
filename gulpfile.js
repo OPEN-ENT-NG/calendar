5dc46df88d328bd74bf1c6dcb78471f0ccdf79ae
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2022-09-27 10:09:40 +0200

    feat(sidebar): #MC-203 add sync button to external calendars (#126 & #137 & #140)

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,12 @@ var bundle = gulp.src('./node_modules/entcore/bundle/*')
 return merge(html, bundle);
 });
 
-gulp.task('webpack', ['copy-files'], () => {
+gulp.task('copy-mdi-font', ['copy-files'], function () {
+    return gulp.src('./node_modules/@mdi/font/fonts/*')
+        .pipe(gulp.dest('./src/main/resources/public/font/material-design/fonts'));
+});
+
+gulp.task('webpack', ['copy-mdi-font'], () => {
     return gulp.src('./src/main/resources/public')
         .pipe(webpack(require('./webpack.config.js')))
         .on('error', function handleError() {
