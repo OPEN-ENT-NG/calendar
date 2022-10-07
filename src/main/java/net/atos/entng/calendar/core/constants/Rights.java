ceba826d4270e316d4947495038b6ae329d8c8a5
alicedraillard <alice.draillard@cgi.com>
2022-10-07 16:49:05 +0200

    feat(sync): #MC-184 get and save calendar + events from url

--- src/main/java/net/atos/entng/calendar/core/constants/Rights.java
+++ src/main/java/net/atos/entng/calendar/core/constants/Rights.java
@@ -2,4 +2,6 @@ package net.atos.entng.calendar.core.constants;
 
 public class Rights {
     public static final String SYNC = "calendar.sync";
+    public static final String UPDATE = "calendar.update";
+    public static final String CHECKUPDATE = "calendar.checkupdate";
 }
