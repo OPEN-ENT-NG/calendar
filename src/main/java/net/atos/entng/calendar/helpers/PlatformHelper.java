02bf27a3115ef8525685a20140e7f0fc7b034201
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2023-01-03 10:10:20 +0100

    feat(zimbra) : #MC-260 check if external calendar has platform (#194)

--- src/main/java/net/atos/entng/calendar/helpers/PlatformHelper.java
+++ src/main/java/net/atos/entng/calendar/helpers/PlatformHelper.java
@@ -1,20 +1,34 @@
 package net.atos.entng.calendar.helpers;
 
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import net.atos.entng.calendar.core.constants.Field;
+import net.atos.entng.calendar.core.enums.ExternalICalEventBusActions;
+import net.atos.entng.calendar.ical.ExternalImportICal;
+import net.atos.entng.calendar.services.PlatformService;
+import net.atos.entng.calendar.services.ServiceFactory;
+import org.entcore.common.mongodb.MongoDbControllerHelper;
+import org.entcore.common.user.UserInfos;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-public class PlatformHelper {
+
+public class PlatformHelper extends MongoDbControllerHelper {
 
     protected static final Logger log = LoggerFactory.getLogger(PlatformHelper.class);
 
-    private PlatformHelper() {
+    private final PlatformService platformService;
+
+    public PlatformHelper(String collection, ServiceFactory serviceFactory) {
+        super(collection, null);
+        this.platformService = serviceFactory.platformService();
     }
 
     /**
@@ -40,4 +54,42 @@ public class PlatformHelper {
         List<Pattern> regexList = createRegexList(platformsList);
         return regexList.stream().anyMatch(regex -> regex.matcher(targetUrl).matches());
     }
+
+    public Future<Boolean> checkCalendarPlatform(UserInfos user, JsonObject calendar) {
+        Promise<Boolean> promise = Promise.promise();
+
+        String url = calendar.getString(Field.ICSLINK, null);
+        String platform = calendar.getString(Field.PLATFORM, null);
+
+        if (!StringsHelper.isNullOrEmpty(url) && StringsHelper.isNullOrEmpty(platform)) {
+            String finalUrl = url.trim();
+            this.platformService.retrieveAll()
+                    .onSuccess(platformList -> promise.complete(PlatformHelper.checkUrlInRegex(finalUrl, platformList)))
+                    .onFailure(error -> {
+                        String message = String.format("[Calendar@%s::checkCalendarPlatform]:  an error has occurred during " +
+                                "platform check: %s", this.getClass().getSimpleName(), error.getMessage());
+                        log.error(message);
+                        promise.fail(error.getMessage());
+                    });
+        } else if (StringsHelper.isNullOrEmpty(url) && !StringsHelper.isNullOrEmpty(platform)) {
+            switch (platform) {
+                case Field.ZIMBRA:
+                    promise.complete(config.getBoolean(Field.ENABLE_ZIMBRA, false));
+                    break;
+                default:
+                    String message = String.format("[Calendar@%s::checkCalendarPlatform]:  an error has occurred during " +
+                            "platform check : platform is not valid", this.getClass().getSimpleName());
+                    log.error(message);
+                    Future.failedFuture(message);
+                    break;
+            }
+        } else {
+            String message = String.format("[Calendar@%s::checkCalendarPlatform]: an error has occurred during " +
+                    "platform check : object is missing data", this.getClass().getSimpleName());
+            Future.failedFuture(message);
+        }
+
+        return promise.future();
+    }
+
 }
---
+++ src/main/java/net/atos/entng/calendar/helpers/StringsHelper.java
@@ -0,0 +1,14 @@
+package net.atos.entng.calendar.helpers;
+
+public class StringsHelper {
+
+    /**
+     * Tells whether the given string is either {@code} or consists solely of whitespace characters.
+     *
+     * @param target string to check
+     * @return {@code true} if the target string is null or empty
+     */
+    public static boolean isNullOrEmpty( String target ) {
+        return target == null || target.isEmpty();
+    }
+}
