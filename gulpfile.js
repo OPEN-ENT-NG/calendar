8406d8c66a9e1f8fe3e382f04c14748fbf785c84
Valentin Mourot <thaledric@gmail.com>
2023-09-29 10:40:40 +0200

    chore: MOZO-117, up migration 4.6

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,102 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
-var merge = require('merge2');
-const replace = require('gulp-replace');
-var clean = require('gulp-clean');
-var argv = require('yargs').argv;
-var fs = require('fs');
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const merge = require("merge2");
+const replace = require("gulp-replace");
+const clean = require("gulp-clean");
+const argv = require("yargs").argv;
+const fs = require("fs");
 
-gulp.task('drop-cache', function(){
-    return gulp.src(['./src/main/resources/public/dist'], { read: false })
-        .pipe(clean());
-});
-
-gulp.task('copy-mdi-font', ['drop-cache'], function () {
-    return gulp.src('./node_modules/@mdi/font/fonts/*')
-        .pipe(gulp.dest('./src/main/resources/public/font/material-design/fonts'));
-});
-
-gulp.task('webpack', ['copy-mdi-font'], () => {
-    return gulp.src('./src/main/resources/public')
-        .pipe(webpack(require('./webpack.config.js')))
-        .on('error', function handleError() {
-            this.emit('end'); // Recover from errors
-        })
-        .pipe(gulp.dest('./src/main/resources/public/dist'));
-});
-
-gulp.task('build', ['webpack'], () => {
-    var refs = gulp.src("./src/main/resources/view-src/**/*.html")
-        .pipe(replace('@@VERSION', Date.now()))
-        .pipe(gulp.dest("./src/main/resources/view"));
-
-var copyBehaviours = gulp.src('./src/main/resources/public/dist/behaviours.js')
-    .pipe(gulp.dest('./src/main/resources/public/js'));
-
-return merge[refs, copyBehaviours];
-});
-
-function getModName(fileContent){
-    var getProp = function(prop){
-        return fileContent.split(prop + '=')[1].split(/\r?\n/)[0];
-    }
-    return getProp('modowner') + '~' + getProp('modname') + '~' + getProp('version');
+function dropCache() {
+  return gulp
+    .src(["./src/main/resources/public/dist"], {
+      read: false,
+      allowEmpty: true,
+    })
+    .pipe(clean());
 }
 
-gulp.task('watch', () => {
-    var springboard = argv.springboard;
-if(!springboard){
-    springboard = '../springboard-open-ent/';
+function buildDev() {
+  return gulp
+    .src("./src/main/resources/public")
+    .pipe(webpack(require("./webpack.config.js")))
+    .on("error", function handleError() {
+      this.emit("end"); // Recover from errors
+    })
+    .pipe(gulp.dest("./src/main/resources/public/dist"));
 }
-if(springboard[springboard.length - 1] !== '/'){
-    springboard += '/';
+
+function build(done) {
+  const refs = gulp
+    .src("./src/main/resources/view-src/**/*.+(html|json)")
+    .pipe(replace("@@VERSION", Date.now()))
+    .pipe(gulp.dest("./src/main/resources/view"));
+
+  const copyBehaviours = gulp
+    .src("./src/main/resources/public/dist/behaviours.js")
+    .pipe(gulp.dest("./src/main/resources/public/js"));
+
+  merge[(refs, copyBehaviours)];
+  done();
 }
 
-gulp.watch('./src/main/resources/public/ts/**/*.ts', () => gulp.start('build'));
+gulp.task("drop-cache", dropCache);
+gulp.task("build-dev", buildDev);
+gulp.task("build", build);
 
-fs.readFile("./gradle.properties", "utf8", function(error, content){
-    var modName = getModName(content);
-    gulp.watch(['./src/main/resources/public/template/**/*.html', '!./src/main/resources/public/template/entcore/*.html'], () => {
-        console.log('Copying resources to ' + springboard + 'mods/' + modName);
-    gulp.src('./src/main/resources/**/*')
-        .pipe(gulp.dest(springboard + 'mods/' + modName));
-});
+function getModName(fileContent) {
+  const getProp = function (prop) {
+    return fileContent.split(prop + "=")[1].split(/\r?\n/)[0];
+  };
+  return (
+    getProp("modowner") + "~" + getProp("modname") + "~" + getProp("version")
+  );
+}
 
-    gulp.watch('./src/main/resources/view/**/*.html', () => {
-        console.log('Copying resources to ' + springboard + 'mods/' + modName);
-    gulp.src('./src/main/resources/**/*')
-        .pipe(gulp.dest(springboard + 'mods/' + modName));
-});
-});
-});
+function watchFiles() {
+  let springboard = argv.springboard;
+  if (!springboard) {
+    springboard = "../springboard-open-ent/";
+  }
+  if (springboard[springboard.length - 1] !== "/") {
+    springboard += "/";
+  }
+
+  gulp.watch(
+    "./src/main/resources/public/ts/**/*.ts",
+    gulp.series("drop-cache", "build-dev", "build")
+  );
+
+  fs.readFile("./gradle.properties", "utf8", function (err, content) {
+    const modName = getModName(content);
+    gulp.watch(["./src/main/resources/public/js"], () => {
+      console.log("Copying resources to " + springboard + "mods/" + modName);
+      gulp
+        .src("./src/main/resources/**/*")
+        .pipe(gulp.dest(springboard + "mods/" + modName));
+    });
+    gulp.watch(
+      [
+        "./src/main/resources/public/template/**/*.html",
+        "!./src/main/resources/public/template/entcore/*.html",
+      ],
+      () => {
+        console.log("Copying resources to " + springboard + "mods/" + modName);
+        gulp
+          .src("./src/main/resources/**/*")
+          .pipe(gulp.dest(springboard + "mods/" + modName));
+      }
+    );
+
+    gulp.watch("./src/main/resources/view/**/*.html", () => {
+      console.log("Copying resources to " + springboard + "mods/" + modName);
+      gulp
+        .src("./src/main/resources/**/*")
+        .pipe(gulp.dest(springboard + "mods/" + modName));
+    });
+  });
+}
+
+gulp.task("watch", watchFiles);
+
+exports.watch = gulp.parallel("watch");
+exports.build = gulp.series("drop-cache", "build-dev", "build");
